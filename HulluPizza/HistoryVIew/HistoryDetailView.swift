24e961c356968a27cc18825fbd3c66d3e2285d45
ragarwal06 <agarwal.rahul324@gmail.com>
2020-11-30 21:29:50 +0530

    Added Maps

--- HulluPizza/HistoryVIew/HistoryDetailView.swift
+++ HulluPizza/HistoryVIew/HistoryDetailView.swift
@@ -8,15 +8,19 @@
 import SwiftUI
 
 struct HistoryDetailView: View {
+    @State var isPresented:Bool = false
     var historyItem: HistoryItem
     @Binding var imageId: Int
     var body: some View {
         imageId = historyItem.id
         return VStack {
             PageTitleView(title: historyItem.name)
-                .padding(.bottom)
+            MapView(latitude: historyItem.latitude, longitude: historyItem.longitude, regionRadius: 1000000)
+                .frame(height: 100)
+            PresentMapButton(isPresented: $isPresented, historyItem: historyItem)
             Text(historyItem.history)
-        }.padding()
+        }
+        
     }
 }
 
---
+++ HulluPizza/HistoryVIew/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MapView.swift
+//  HulluPizza
+//
+//  Created by Rahul Agarwal on 30/11/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    var latitude: Double
+    var longitude: Double
+    var regionRadius: Double
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let mapView = MKMapView()
+        view.frame = view.frame
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        view.setRegion(MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius),animated: true)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+}
+
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
